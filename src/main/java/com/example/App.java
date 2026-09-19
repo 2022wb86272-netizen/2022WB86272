d163e5da99b5ff3e84d42c555f123231897f7d8c
2022WB86272 <2022wb86272@wilp.bits-pilani.ac.in>
2026-09-19 17:58:08 +0530

    Modified source code for continuous integration

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from RIYA Rose Saji - 2022WB86272");
+System.out.println("Continuous Integration using Jenkins");
     }
 }
